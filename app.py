7f8a66c2659c603d1f2df16d891045048ac49837
Jes Fink-Jensen <jes@finkjensen.be>
2019-05-25 11:33:04 +0200

    Added / route and entry point

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 
+@app.route('/')
+def index():
+    return jsonify({"status": "ok"})
+
 @app.route('/submit')
 def submit():
     predict.predict_and_submit()
@@ -15,3 +19,7 @@ def submit():
 def train():
     t.train()
     return jsonify({"status": "ok"})
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug = True, threaded=False, processes=1)

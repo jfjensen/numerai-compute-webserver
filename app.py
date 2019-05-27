5bf62bf1a4fecbdc6d364313bd4ee1a3ff22e612
Jes Fink-Jensen <jes@finkjensen.be>
2019-05-27 13:09:44 +0200

    Add Waitress server for production

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import predict
 import train as t
 import status as stat
-
+from waitress import serve
 from flask import Flask, jsonify
 app = Flask(__name__)
 
+production = True
 
 @app.route('/')
 def index():
@@ -23,5 +24,11 @@ def train():
 
 if __name__ == "__main__":
     stat.set_ready()
-    # app.run(host='0.0.0.0', debug = True) #, threaded=False, processes=1)
-    app.run(host='0.0.0.0', debug = True, threaded=True, processes=1)
+
+    if production:
+        serve(app, host='0.0.0.0', port=5000)
+    else:
+        # app.run(host='0.0.0.0', debug = True) #, threaded=False, processes=1)
+        app.run(host='0.0.0.0', debug = True, threaded=True, processes=1)
+    
+ 
